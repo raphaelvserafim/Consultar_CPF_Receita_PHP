59c12a388de5a2fdba810253f096c2bf236b7abf
Raphael Serafim <68257896+raphaelvserafim@users.noreply.github.com>
2021-12-31 17:01:39 -0400

    Create exemplos.php

---
+++ exemplos.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+include 'vendor/autoload.php'; 
+
+
+use Cachesistemas\Consultacpf;
+
+
+ $consulta      = new ConsultaCPF();
+
+// $result      = $consulta->imgRecaptchaCPF();
+// echo json_encode($result);
+
+ 
+// $result      = $consulta->consultandoCPF('00000000000','dd/mm/YYYY', 'captcha');
+// echo json_encode($result);
+
+ 
